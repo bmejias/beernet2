b555ddfe2d3b20770a2a8e0a2ebccee48c119039
Boriss Mejias <tchorix@gmail.com>
2013-03-02 01:03:24 +0100

    first working version of the script to create functor skeletons.... now it needs to handle better the arguments

--- tools/new_functor.py
+++ tools/new_functor.py
@@ -1,38 +1,30 @@
 #! /usr/bin/env python
 
 import sys
+import os
+
+pwd         = os.getcwd()
+SRC_PATH    = 'beernet2/src/'
+package     = pwd[(pwd.find(SRC_PATH) + len(SRC_PATH)):]
+
+script_full = os.path.realpath(__file__)
+script_dir  = script_full[:script_full.rfind('/') + 1]
+header_fname= script_dir + "header.txt"
+header = open(header_fname, 'r')
 
 functor_name = sys.argv[1]
-functor_path = '.'
-if len(sys.argv) == 3:
-    functor_path = sys.argv[2]
-functor_file_name = functor_path + '/' + functor_name + '.oz'
-# functor = open(functor_file_name, 'w')
-header = """
-/*-------------------------------------------------------------------------
- *
- * %s
- *
- *  <Short Description>
- *
- * LICENSE
- *
- *    Beernet is released under the Beerware License (see file LICENSE) 
- * 
- * IDENTIFICATION 
- *
- *    Author: $Author$
- *
- *    Last change: $Revision$ $Author$
- *
- *    $Date$
- *
- * NOTES
- *      
- *  <If any>
- *
- *-------------------------------------------------------------------------
- */""" % functor_file_name
-header = header.lstrip()
-print header
+if functor_name[-3:] != '.oz':
+    functor_name += '.oz'
+functor_full_name = package + '/' + functor_name
+functor_path_name = pwd + '/' + functor_full_name
+functor = open(functor_name, 'w')
+
+for line in header:
+    new_line = line
+    if line.find('__filename__') > 0:
+        new_line = line.replace('__filename__', functor_full_name)
+    functor.write(new_line)
+
+functor.close()
+    
 
